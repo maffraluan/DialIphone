70db0cf1fb634c28801cfe3416d154a75be908b1
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2020-09-14 21:03:15 -0300

    Initial commit with tsx

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# Iphone Dial clone. 
